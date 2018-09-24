68bb9b1f15bc90b1a045ff8cd379c8d4e30f5210
Jordan Stremming <techzune@operontech.com>
2018-09-23 22:54:26 -0500

    Updated version number

--- src/main/java/com/jstremming/categoro/util/Updater.java
+++ src/main/java/com/jstremming/categoro/util/Updater.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 
 public class Updater {
-	private static final String VERSION = "1.2";
+	private static final String VERSION = "1.3";
 
 	public static boolean isNewVersion() {
 		try {
